730247df207eea9ce468c8fbe7482a399e5c7ed0
Mohit_Jangra <mohitjangra6017@gmail.com>
2022-03-20 00:35:14 +0530

    Add CommonMethod.py

---
+++ CommonMethod/CommonMethod.py
@@ -0,0 +1,14 @@
+
+import string
+import random
+
+
+class CommonMethod:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def random_generator(size=8, chars=string.ascii_lowercase + string.digits):
+        return ''.join(random.choice(chars) for x in range(size))
+
+
+
